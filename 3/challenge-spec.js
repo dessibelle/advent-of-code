3c9c0e74d32c9fd495d46a2924c23a1fa3509333
Simon Fransson <simon@funnel.io>
2017-12-15 16:59:00 +0100

    challenge 3 part 1

--- 3/challenge-spec.js
+++ 3/challenge-spec.js
@@ -2,9 +2,24 @@ const challenge = require('./challenge.js');
 
 describe('challenge', () => {
   describe('first part', () => {
-    it('can find repeating digit', () => {
-      // const result = challenge.solve();
-      // expect(result).toBe(1234);
+    it('1 is carried 0 steps, since it\'s at the access port', () => {
+      const result = challenge.solve(1);
+      expect(result).toBe(0);
+    });
+
+    it('12 is carried 3 steps, such as: down, left, left', () => {
+      const result = challenge.solve(12);
+      expect(result).toBe(3);
+    });
+
+    it('23 is carried only 2 steps: up twice', () => {
+      const result = challenge.solve(23);
+      expect(result).toBe(2);
+    });
+
+    it('1024 must be carried 31 steps', () => {
+      const result = challenge.solve(1024);
+      expect(result).toBe(31);
     });
   });
 
--- 3/challenge.js
+++ 3/challenge.js
@@ -1,9 +1,34 @@
 const fs = require('fs');
 const lodash  = require('lodash');
 
+function layerForNumItems(value) {
+  return value ? Math.ceil((Math.sqrt(value) + 1) / 2) : 0;
+}
 
-function solve() {
+function numItemsForLayer(value) {
+  return value ? Math.pow(2 * value - 1, 2) : 0;
+}
 
+function manhattanDistance(value) {
+  const layer = layerForNumItems(value);
+  const layersToTravel = layer - 1;
+
+  const itemsInLayer = numItemsForLayer(layer) - numItemsForLayer(layer - 1);
+  const offsetInLayer = value - numItemsForLayer(layer - 1);
+
+  const itemsOnSide = parseInt(itemsInLayer / 4, 10);
+  const offsetOnSide = itemsOnSide ? offsetInLayer % itemsOnSide : 0;
+
+  const sideCenterIdx = parseInt(itemsOnSide / 2, 10) + 1;
+  const positionOnSide = offsetOnSide + 1;
+
+  const distanceToCenter = Math.abs(sideCenterIdx - positionOnSide);
+
+  return layersToTravel + distanceToCenter;
+}
+
+function solve(value) {
+  return manhattanDistance(value);
 }
 
 function complete() {
@@ -12,10 +37,12 @@ function complete() {
 
 function run() {
   fs.readFile('input', 'utf8', (err, data) => {
-    const answer1 = solve(data);
+    const squareId = parseInt(data, 10);
+
+    const answer1 = solve(squareId);
     process.stdout.write(`Answer 1: ${answer1}\n`);
 
-    const answer2 = complete(data);
+    const answer2 = complete(squareId);
     process.stdout.write(`Answer 2: ${answer2}\n`);
   });
 }
