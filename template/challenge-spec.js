eb3f7496dc199ac66090adb6c4e7a3093a018574
Simon Fransson <simon@dessibelle.se>
2017-12-15 07:38:14 +0100

    added template

---
+++ template/challenge-spec.js
@@ -0,0 +1,17 @@
+const challenge = require('./challenge.js');
+
+describe('challenge', () => {
+  describe('first part', () => {
+    it('can find repeating digit', () => {
+      // const result = challenge.solve();
+      // expect(result).toBe(1234);
+    });
+  });
+
+  describe('second part', () => {
+    it('can find repeating digit', () => {
+      // const result = challenge.solve();
+      // expect(result).toBe(1234);
+    });
+  });
+});
