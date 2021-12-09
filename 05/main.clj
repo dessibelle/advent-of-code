4e69cd93b58fde8e3d09bf60b908e6f2af73e487
Simon Fransson <simon.fransson@funnel.io>
2021-12-09 14:23:24 +0100

    day 5 part 2

--- 05/main.clj
+++ 05/main.clj
@@ -1,10 +1,10 @@
-(ns user (:require [clojure.string :as s])
+(ns user (:require [clojure.string :as s]))
 
 (defn parse-coordinate [coord-string]
   (-> coord-string
-    (s/split #",")
-    ((partial map #(Integer. %)))
-    (#(hash-map :x (first %) :y (second %)))))
+      (s/split #",")
+      ((partial map #(Integer. %)))
+      (#(hash-map :x (first %) :y (second %)))))
 
 (defn parse-vector [vector-string]
   (-> vector-string
@@ -17,81 +17,55 @@
     (or (apply = x-coords)
         (apply = y-coords))))
 
-(defn get-bounds [vectors]
-  (let [coords (apply concat vectors)
-        x-coords (map :x coords)
-        y-coords (map :y coords)]
-    (list {:x (apply min x-coords)
-           :y (apply min y-coords)}
-          {:x (apply max x-coords)
-           :y (apply max y-coords)})))
+(defn diagonal-line? [vector]
+  (let [x-coords (map :x vector)
+        y-coords (map :y vector)
+        cathetus-size #(apply - (sort-by identity > %))
+        x-size (cathetus-size x-coords)
+        y-size (cathetus-size y-coords)]
+    (= x-size y-size)))
 
-(defn get-bounds-size [bounds key]
-  (let [coords (map key bounds)]
-    ;; (- (inc (apply max coords)) (apply min coords))
-    (inc (apply max coords))))
+(defn vector-range [v1 v2]
+  (let [largest-first (> v1 v2)
+        val1 (if largest-first (inc v1) v1)
+        val2 (if largest-first v2 (inc v2))]
+    (if largest-first
+      (reverse (range val2 val1))
+      (range val1 val2))))
 
-(defn create-board [bounds]
-  (let [x-size (get-bounds-size bounds :x)
-        y-size (get-bounds-size bounds :y)
-        empty-row (repeat x-size 0)]
-    (println bounds)
-    (println "using board of" x-size "x" y-size)
-    (repeat y-size empty-row)))
+(defn interpolate-vector [vector]
+  (let [p1 (first vector)
+        p2 (last vector)
+        x1 (:x p1) x2 (:x p2)
+        y1 (:y p1) y2 (:y p2)
+        x-length (apply - (sort-by identity > [x1 x2]))
+        y-length (apply - (sort-by identity > [y1 y2]))
+        x-vals (if (= x-length 0) (repeat (inc y-length) x1) (vector-range x1 x2))
+        y-vals (if (= y-length 0) (repeat (inc x-length) y1) (vector-range y1 y2))
+        points (map list x-vals y-vals)]
+    points))
 
-(defn perpendicular-to-axis? [axis vector]
-  (apply = (map axis vector)))
-
-(defn apply-vector [row start end]
-  (let [row-len (count row)
-        vector-len (- (+ 1 end) start)
-        addition  (concat (repeat start 0) (repeat vector-len 1) (repeat (- row-len vector-len) 0))]
-    (map + row addition)))
-
-(defn apply-vector-to-board [board vector col-key row-key]
-  (let [y (row-key (first vector))
-        start (apply min (map col-key vector))
-        end (apply max (map col-key vector))]
-    (map-indexed (fn [idx row] (if (= idx y) (apply-vector row start end) row)) board)))
-
-(defn transpose-board [& xs]
-  (apply map list xs))
-
-(defn apply-vectors [board vectors]
-  (let [horizontal-lines (filter (partial perpendicular-to-axis? :y) vectors)
-        vertical-lines (filter (partial perpendicular-to-axis? :x) vectors)
-        state (reduce (fn [coll vector] (apply-vector-to-board coll vector :x :y)) board horizontal-lines)
-        state (apply transpose-board state) ;; transpose cols to rows
-        state (reduce (fn [coll vector] (apply-vector-to-board coll vector :y :x)) state vertical-lines)
-        state (apply transpose-board state)]  ;; transpose rows back to cols
-    state))
-
-(defn format-row [board-row]
-  (s/join "" (map #(if (= 0 %) "." %) board-row)))
-
-(defn format-board [board]
-  (s/join "\n" (map format-row board)))
-
-(defn calculate-score [board]
-  (apply + (map (fn [row] (count (filter (partial < 1) row))) board)))
+(defn vectors-to-points [vectors]
+  (map interpolate-vector vectors))
 
 (defn readfile [filename]
   (->> filename
        slurp
        s/split-lines))
 
-(defn solve [filename]
+(defn solve [filename part]
   (let [input-data (readfile filename)
         vectors (map parse-vector input-data)
         straight-lines (filter straight-line? vectors)
-        bounds (get-bounds straight-lines)
-        board (create-board bounds)
-        state (apply-vectors board vectors)
-        score (calculate-score state)]
-    (println bounds)
-    ;; (println (format-board state))
-    score))
-    
-(solve "./test")
+        diagonal-lines (if (= part 1) '() (filter diagonal-line? vectors))
+        points-by-vector (vectors-to-points (concat straight-lines diagonal-lines))
+        points-by-frequency (frequencies (apply concat points-by-vector))
+        points (filter (fn [[_ val]] (< 1 val)) points-by-frequency)]
+    (count points)
+    ))
 
-(solve "./input")
+;; (solve "./test" 1)
+;; (solve "./test" 2)
+
+(solve "./input" 1)
+(solve "./input" 2)
