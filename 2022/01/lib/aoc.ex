c0f3c225ae930075f17361fdff718811af0e5d03
Simon Fransson <simon.fransson@funnel.io>
2022-12-03 22:10:58 +0100

    add days 01, 02 and 03

---
+++ 2022/01/lib/aoc.ex
@@ -0,0 +1,28 @@
+defmodule AOC do
+  @moduledoc """
+  Documentation for `AOC`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AOC.hello()
+      :world
+
+  """
+  def start(_type, _args) do
+    IO.puts "Hello World"
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: A.Worker.start_link(arg)
+      # {A.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: A.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
