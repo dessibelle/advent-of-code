962303a27cf77e208ce2913b206f4ff53ce73fc2
Simon Fransson <simon.fransson@funnel.io>
2022-12-05 00:21:05 +0100

    solve day 2 part 1

--- 2022/02/lib/aoc.ex
+++ 2022/02/lib/aoc.ex
@@ -14,17 +14,66 @@ defmodule AOC do
   end
 
   def parse_input(raw_input) do
-    String.split(raw_input)
+    String.split(String.trim(raw_input), "\n")
+    |> Enum.map(fn round ->
+      String.split(round)
+      |> Enum.map(&String.to_atom/1)
+      |> List.to_tuple()
+    end)
+  end
+
+  def move_idx(move) do
+    cond do
+      Enum.member?([:A, :X], move) ->
+        1
+      Enum.member?([:B, :Y], move) ->
+        2
+      Enum.member?([:C, :Z], move) ->
+        3
+    end
+  end
+
+  def beats?(our_move, opponent_move) do
+    # Rock       :X / :A / 1
+    # Paper      :Y / :B / 2
+    # Scissors   :Z / :C / 3
+    our_move_idx = move_idx(our_move)
+    opponent_move_idx = move_idx(opponent_move)
+    # TODO: Can use some -1 and modulo math instead
+    cond do
+      our_move_idx == 1 && opponent_move_idx == 3 ->
+        true
+      our_move_idx == 2 && opponent_move_idx == 1 ->
+        true
+      our_move_idx == 3 && opponent_move_idx == 2 ->
+        true
+      true ->
+        false
+    end
+  end
+
+  def score({opponent_move, our_move}) do
+    move_points = move_idx(our_move)
+    cond do
+      beats?(our_move, opponent_move) ->
+        6 + move_points
+      beats?(opponent_move, our_move) ->
+        0 + move_points
+      true ->
+        3 + move_points
+      end
   end
 
   def load_input(path) do
     raw_input = read_input(path)
-    parse_input(raw_input)
+    rounds = parse_input(raw_input)
   end
 
   def start(_type, _args) do
     data = load_input("./input")
-    IO.puts(inspect(data))
+    score = Enum.map(data, &AOC.score/1)
+    |> Enum.sum()
+    IO.puts(inspect(score))
 
     # List all child processes to be supervised
     children = [
