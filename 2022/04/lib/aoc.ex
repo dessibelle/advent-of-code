6ddb4f0b0fc363c98889d96bfc8fb953ae63773c
Simon Fransson <simon.fransson@funnel.io>
2022-12-04 23:14:42 +0100

    add day 4

---
+++ 2022/04/lib/aoc.ex
@@ -0,0 +1,40 @@
+defmodule AOC do
+  @moduledoc """
+  Advent of Code solution
+  """
+
+
+  def read_input(path) do
+    {status, data} = File.read(path)
+    if status == :ok do
+      data
+    else
+      raise "Input data not found"
+    end
+  end
+
+  def parse_input(raw_input) do
+    String.split(raw_input)
+  end
+
+  def load_input(path) do
+    raw_input = read_input(path)
+    parse_input(raw_input)
+  end
+
+  def start(_type, _args) do
+    data = load_input("./input")
+    IO.puts(inspect(data))
+
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: A.Worker.start_link(arg)
+      # {A.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: A.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
