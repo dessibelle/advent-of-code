27fe17d43cb154e696d806fda4919b91dcaf61b2
Simon Fransson <simon.fransson@funnel.io>
2022-12-05 14:20:23 +0100

    simplify model and just use ints instead

--- 2022/02/lib/aoc.ex
+++ 2022/02/lib/aoc.ex
@@ -13,107 +13,64 @@ defmodule AOC do
     end
   end
 
+    def move_idx(move) do
+      cond do
+        Enum.member?(["A", "X"], move) ->
+          0
+        Enum.member?(["B", "Y"], move) ->
+          1
+        Enum.member?(["C", "Z"], move) ->
+          2
+      end
+    end
+
   def parse_input(raw_input) do
     String.split(String.trim(raw_input), "\n")
     |> Enum.map(fn round ->
       String.split(round)
-      |> Enum.map(&String.to_atom/1)
+      |> Enum.map(&AOC.move_idx/1)
       |> List.to_tuple()
     end)
   end
 
-  def move_idx(move) do
-    cond do
-      Enum.member?([:A, :X], move) ->
-        1
-      Enum.member?([:B, :Y], move) ->
-        2
-      Enum.member?([:C, :Z], move) ->
-        3
-    end
-  end
-
   def get_losing_move(opponent_move) do
-    # TODO: Can use some +1 and modulo math instead
-    cond do
-      opponent_move == :A ->
-        :C
-      opponent_move == :B ->
-        :A
-      opponent_move == :C ->
-        :B
-    end
+    Integer.mod(opponent_move - 1, 3)
   end
 
   def get_draw_move(opponent_move) do
-    # TODO: Can use == math instead
-    cond do
-      opponent_move == :A ->
-        :A
-      opponent_move == :B ->
-        :B
-      opponent_move == :C ->
-        :C
-    end
+    opponent_move
   end
 
   def get_winning_move(opponent_move) do
-    # TODO: Can use some -1 and modulo math instead
-    cond do
-      opponent_move == :A ->
-        :B
-      opponent_move == :B ->
-        :C
-      opponent_move == :C ->
-        :A
-    end
+    Integer.mod(opponent_move + 1, 3)
   end
 
   def beats?(our_move, opponent_move) do
     # Rock       :X / :A / 1
     # Paper      :Y / :B / 2
     # Scissors   :Z / :C / 3
-    our_move_idx = move_idx(our_move)
-    opponent_move_idx = move_idx(opponent_move)
-    # TODO: Can use some -1 and modulo math instead
-    cond do
-      our_move_idx == 1 && opponent_move_idx == 3 ->
-        true
-      our_move_idx == 2 && opponent_move_idx == 1 ->
-        true
-      our_move_idx == 3 && opponent_move_idx == 2 ->
-        true
-      true ->
-        false
-    end
+    Integer.mod(our_move - opponent_move, 3) == 1
   end
 
   def score_p1({opponent_move, our_move}) do
-    move_points = move_idx(our_move)
     cond do
       beats?(our_move, opponent_move) ->
-        6 + move_points
+        6 + our_move + 1
       beats?(opponent_move, our_move) ->
-        0 + move_points
+        0 + our_move + 1
       true ->
-        3 + move_points
+        3 + our_move + 1
       end
   end
 
   def score_p2({opponent_move, outcome}) do
     cond do
-      outcome == :X ->
-        our_move = get_losing_move(opponent_move)
-        move_points = move_idx(our_move)
-        0 + move_points
-      outcome == :Y ->
-        our_move = get_draw_move(opponent_move)
-        move_points = move_idx(our_move)
-        3 + move_points
-      outcome == :Z ->
-        our_move = get_winning_move(opponent_move)
-        move_points = move_idx(our_move)
-        6 + move_points
+      outcome == 0 ->
+        0 + get_losing_move(opponent_move) + 1
+      outcome == 1 ->
+        3 + get_draw_move(opponent_move) + 1
+      outcome == 2 ->
+        6 + get_winning_move(opponent_move) + 1
       end
   end
 
@@ -135,7 +92,7 @@ defmodule AOC do
   end
 
   def start(_type, _args) do
-    score = solve("./input", 2)
+    score = solve("./input", 1)
     IO.puts(inspect(score))
 
     # List all child processes to be supervised
