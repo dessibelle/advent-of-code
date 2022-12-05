02745971c14e5eafc1e1d7541d38a7d8179b6041
Simon Fransson <simon.fransson@funnel.io>
2022-12-05 15:38:23 +0100

    solve day 3 part 1

--- 2022/03/lib/aoc.ex
+++ 2022/03/lib/aoc.ex
@@ -15,6 +15,7 @@ defmodule AOC do
 
   def parse_input(raw_input) do
     String.split(raw_input)
+    |> Enum.map(&AOC.compartmentalize_rucksack_contents/1)
   end
 
   def load_input(path) do
@@ -22,9 +23,39 @@ defmodule AOC do
     parse_input(raw_input)
   end
 
+  def compartmentalize_rucksack_contents(contents) do
+    String.split_at(contents, Integer.floor_div(String.length(contents), 2))
+    |> Tuple.to_list()
+    |> Enum.map(fn compartment -> MapSet.new(String.to_charlist(compartment)) end)
+    |> List.to_tuple()
+  end
+
+  def get_common_items({first, second}) do
+    MapSet.intersection(first, second)
+  end
+
+  def get_item_priority(item) do
+    if item >= ?a do
+      item - ?a + 1
+    else
+      item - ?A + 1 + 26
+    end
+  end
+
+  def solve(path, 1) do
+    load_input(path)
+    |> Enum.map(&AOC.get_common_items/1)
+    |> Enum.map(fn items ->
+      MapSet.to_list(items)
+      |> Enum.map(&AOC.get_item_priority/1)
+      |> Enum.reduce(0, fn priority, acc -> priority + acc end)
+    end)
+    |> Enum.sum()
+  end
+
   def start(_type, _args) do
-    data = load_input("./input")
-    IO.puts(inspect(data))
+    sum = solve("./input", 1)
+    IO.puts(sum |> inspect(charlists: :as_lists))
 
     # List all child processes to be supervised
     children = [
