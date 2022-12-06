b212accc50c565a25825ff17543fb3acaacfbff2
Simon Fransson <simon.fransson@funnel.io>
2022-12-06 22:39:09 +0100

    solve day 5 part 1

--- 2022/lib/day05.ex
+++ 2022/lib/day05.ex
@@ -14,7 +14,6 @@ defmodule AOC.Day05 do
     {index, rows} = String.split(raw_stacks, "\n")
     |> List.pop_at(-1)
 
-    IO.inspect(index)
     size = index
     |> String.at( -2)
     |> String.to_integer()
@@ -40,6 +39,7 @@ defmodule AOC.Day05 do
     end)
   end
 
+  # TODO: adjust from/to for zero-indexed lists
   def parse_operation(raw_operation) do
     matches = Regex.named_captures(~r/move (?<amount>\d+) from (?<from>\d+) to (?<to>\d+)/, raw_operation)
     for {key, val} <- matches, into: %{}, do: {String.to_atom(key), String.to_integer(val)}
@@ -63,8 +63,42 @@ defmodule AOC.Day05 do
     {stacks, operations}
   end
 
+  def apply_operation(stacks, %{:amount => amount, :from => from, :to => to}) do
+    cond do
+      from == to -> stacks
+      true ->
+        # TODO: This could be reflected in parse_operation instead
+        from_idx = from - 1
+        to_idx = to - 1
+        {from_stack, moved_items} = stacks
+        |> Enum.at(from - 1)
+        |> Enum.split(amount * -1)
+
+        to_stack = stacks
+        |> Enum.at(to - 1)
+        |> Enum.concat(moved_items |> Enum.reverse())
+
+        List.replace_at(stacks, from_idx, from_stack)
+        |> List.replace_at(to_idx, to_stack)
+    end
+  end
+
+  def apply_operations({stacks, operations}) do
+    Enum.reduce(operations, stacks, fn operation, acc ->
+      AOC.Day05.apply_operation(acc, operation)
+    end)
+  end
+
+  def top_level_items(stacks) do
+    stacks
+    |> Enum.map(fn stack -> List.last(stack, nil) end)
+  end
+
   def solve(raw_input, 1) do
     parse_input(raw_input)
+    |> apply_operations()
+    |> top_level_items()
+    |> Enum.join("")
   end
 
   def solve(raw_input, 2) do
