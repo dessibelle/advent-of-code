47633ad3c15b8e3682bedebe12200f497c0b003b
Simon Fransson <simon.fransson@funnel.io>
2022-12-06 11:16:08 +0100

    streamline day02

--- 2022/02/day02.ex
+++ 2022/02/day02.ex
@@ -1,8 +1,4 @@
-defmodule AOC do
-  @moduledoc """
-  Advent of Code solution
-  """
-
+defmodule Day02 do
 
   def read_input(path) do
     {status, data} = File.read(path)
@@ -28,7 +24,7 @@ defmodule AOC do
     String.split(String.trim(raw_input), "\n")
     |> Enum.map(fn round ->
       String.split(round)
-      |> Enum.map(&AOC.move_idx/1)
+      |> Enum.map(&Day02.move_idx/1)
       |> List.to_tuple()
     end)
   end
@@ -75,35 +71,34 @@ defmodule AOC do
   end
 
   def load_input(path) do
-    raw_input = read_input(path)
-    rounds = parse_input(raw_input)
+    read_input(path)
+    |> parse_input()
   end
 
   def solve(path, 1) do
-    rounds = load_input(path)
-    score = Enum.map(rounds, &AOC.score_p1/1)
+    load_input(path)
+    |> Enum.map(&Day02.score_p1/1)
     |> Enum.sum()
   end
 
   def solve(path, 2) do
-    rounds = load_input(path)
-    score = Enum.map(rounds, &AOC.score_p2/1)
+    load_input(path)
+    |> Enum.map(&Day02.score_p2/1)
     |> Enum.sum()
   end
 
-  def start(_type, _args) do
-    score = solve("./input", 1)
-    IO.puts(inspect(score))
+  def print() do
+    solutions = %{
+      test1: solve("./test", 1),
+      test2: solve("./test", 2),
+      part1: solve("./input", 1),
+      part2: solve("./input", 2),
+    }
 
-    # List all child processes to be supervised
-    children = [
-      # Starts a worker by calling: A.Worker.start_link(arg)
-      # {A.Worker, arg},
-    ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: A.Supervisor]
-    Supervisor.start_link(children, opts)
+    for {k, x} <- solutions do
+      IO.puts("#{k}: #{x}")
+    end
   end
 end
+
+Day02.print()
