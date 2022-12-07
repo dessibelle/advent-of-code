978f2c1963c533b8212cdd0dab17f2b44e98e9a0
Simon Fransson <simon.fransson@funnel.io>
2022-12-07 09:04:40 +0100

    add tests for actual input too

--- 2022/.template_test.exs
+++ 2022/.template_test.exs
@@ -4,13 +4,23 @@ defmodule AOCTest.Day00Test do
 
   doctest Day
 
-  @input Input.read!(00, "test")
+  @test Input.read!(00, "test")
 
-  test "Part 1" do
-    assert Day.solve(@input, 1) == -1
+  test "Part 1 (example)" do
+    assert Day.solve(@test, 1) == -1
   end
 
-  test "Part 2" do
-    assert Day.solve(@input, 2) == -1
+  test "Part 2 (example)" do
+    assert Day.solve(@test, 2) == -1
   end
+
+  # @input Input.read!(00, "input")
+
+  # test "Part 1" do
+  #   assert Day.solve(@input, 1) == -1
+  # end
+
+  # test "Part 2" do
+  #   assert Day.solve(@input, 2) == -1
+  # end
 end
