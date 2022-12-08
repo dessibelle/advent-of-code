58c9eefa42f72a99e9a644a6264076533f43770e
Simon Fransson <simon.fransson@funnel.io>
2022-12-08 10:35:59 +0100

    basic file size tracking

--- 2022/lib/day07.ex
+++ 2022/lib/day07.ex
@@ -10,7 +10,9 @@ defmodule AOC.Day07 do
   end
 
   def parse_input(raw_input) do
-    String.split(raw_input, "\n")
+    raw_input
+    |> String.trim()
+    |> String.split("\n")
     |> Enum.map(&AOC.Day07.parse_line/1)
   end
 
@@ -25,21 +27,42 @@ defmodule AOC.Day07 do
     end
   end
 
+  def prepend(str, prepend), do: "#{prepend}#{str}"
+  def format_pwd(pwd) do
+    pwd
+    |> Enum.reverse()
+    |> Enum.join("/")
+    |> prepend("/")
+  end
+
   def process_input(input) do
     input
     |> Enum.reduce({%{}, []}, fn {type, data}, {tree, pwd} ->
       cond do
         type == :command && List.first(data) == "cd" ->
           {tree, cd(pwd, Enum.at(data, 1))}
+        type == :output && List.first(data) != "dir" ->
+          file_size = data
+            |> List.first()
+            |> String.to_integer()
+          {Map.update(tree, format_pwd(pwd), file_size, &(&1 + file_size)), pwd}
         true ->
           {tree, pwd}
       end
     end)
   end
 
+  def filter_dirs({tree, _}, filter_fn) do
+    tree
+    |> Map.filter(filter_fn)
+  end
+
   def solve(raw_input, 1) do
     parse_input(raw_input)
     |> process_input()
+    |> filter_dirs(fn {_path, size} -> size <= 100000 end)
+    |> Map.values()
+    |> Enum.sum()
   end
 
   def solve(raw_input, 2) do
