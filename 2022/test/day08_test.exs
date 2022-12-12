15003958a81b35da3a45c49fc0a59a99c5af71ab
Simon Fransson <simon.fransson@funnel.io>
2022-12-12 09:51:33 +0100

    update tests for day 8

--- 2022/test/day08_test.exs
+++ 2022/test/day08_test.exs
@@ -11,7 +11,7 @@ defmodule AOCTest.Day08Test do
   end
 
   test "Part 2 (example)" do
-    assert Day.solve(@test, 2) == -1
+    assert Day.solve(@test, 2) == 8
   end
 
   @input Input.read!(08, "input")
@@ -21,6 +21,6 @@ defmodule AOCTest.Day08Test do
   end
 
   test "Part 2" do
-    assert Day.solve(@input, 2) == -1
+    assert Day.solve(@input, 2) == 313200
   end
 end
