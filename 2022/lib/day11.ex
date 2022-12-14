efaad3d38ae8af4a92151147a5552f22327868c9
Simon Fransson <simon.fransson@funnel.io>
2022-12-14 16:18:43 +0100

    solve day 11 part 1

--- 2022/lib/day11.ex
+++ 2022/lib/day11.ex
@@ -31,22 +31,23 @@ defmodule AOC.Day11 do
     items = Regex.scan(digits_pattern, items_str) |> List.flatten() |> Enum.map(&String.to_integer/1)
     operation_matches = Regex.scan(~r/new = (old|\d+) ([+*\/-]) (old|\d+)/, op_str) |> List.flatten()
     divisible_by = Regex.scan(digits_pattern, test_str) |> List.flatten() |> List.first() |> String.to_integer()
-    truthy_val = Regex.scan(digits_pattern, truthy_str) |> List.flatten() |> List.first() |> String.to_integer()
-    falsy_val = Regex.scan(digits_pattern, falsy_str) |> List.flatten() |> List.first() |> String.to_integer()
+    truthy_idx = Regex.scan(digits_pattern, truthy_str) |> List.flatten() |> List.first() |> String.to_integer()
+    falsy_idx = Regex.scan(digits_pattern, falsy_str) |> List.flatten() |> List.first() |> String.to_integer()
 
     [operation_str, l, op, r] = operation_matches
     operation = parse_operation(l, op, r)
 
     %{
       items: items,
-      truthy_val: truthy_val,
-      falsy_val: falsy_val,
+      truthy_idx: truthy_idx,
+      falsy_idx: falsy_idx,
       divisible_by: divisible_by,
       test: &(Integer.mod(&1, divisible_by) == 0),
-      operation_str: operation_str,
       operation: operation,
+      operation_str: operation_str,
       l: l,
       r: r,
+      op: op,
     }
   end
 
@@ -55,10 +56,115 @@ defmodule AOC.Day11 do
     |> String.trim()
     |> String.split("\n\n")
     |> Enum.map(&AOC.Day11.parse_monkey/1)
+    |> Enum.with_index()
+  end
+
+  def take_turn(monkeys, monkey_idx) do
+    {monkey, _} = Enum.at(monkeys, monkey_idx)
+
+    IO.puts("Monkey #{monkey_idx}: (#{Enum.join(monkey.items, ", ")})")
+
+    inspected_items = Enum.map(monkey.items, fn item ->
+      IO.puts("  Monkey inspects an item with a worry level of #{item}.")
+      # Integer.floor_div(monkey.operation.(item), 3)
+
+      monkey.operation.(item)
+      |> then(fn l ->
+        IO.puts("    Worry level #{monkey.op} by #{monkey.r} to #{l}.")
+        l
+      end)
+      |> Integer.floor_div(3)
+      |> then(fn l ->
+        IO.puts("    Monkey gets bored with item. Worry level is divided by 3 to to #{l}.")
+        l
+      end)
+    end)
+
+    # TODO: Store inspection results on monkey
+
+    {truthy_items, falsy_items} = Enum.split_with(inspected_items, monkey.test)
+
+    # IO.puts("#{inspect(monkey.items, charlists: :as_lists)} => #{inspect(inspected_items, charlists: :as_lists)} => #{inspect(truthy_items, charlists: :as_lists)} + #{inspect(falsy_items, charlists: :as_lists)}")
+
+
+    0..(length(monkeys) - 1)
+    |> Enum.reduce(monkeys, fn idx, acc ->
+      {m, _} = Enum.at(acc, idx)
+      cond do
+        idx == monkey_idx ->
+          Map.put(m, :items, [])
+          |> Map.update(:inspected_items, inspected_items, fn i -> i ++ inspected_items end)
+        idx == monkey.truthy_idx ->
+          truthy_items
+          |> Enum.map(fn i ->
+            IO.puts("  Current worry level is divisible by #{monkey.divisible_by}.")
+            IO.puts("  Item with worry level #{i} is thrown to monkey #{monkey.truthy_idx}.")
+          end)
+          Map.update(m, :items, truthy_items, fn e -> e ++ truthy_items end)
+        idx == monkey.falsy_idx ->
+          falsy_items
+          |> Enum.map(fn i ->
+            IO.puts("  Current worry level is not divisible by #{monkey.divisible_by}.")
+            IO.puts("  Item with worry level #{i} is thrown to monkey #{monkey.falsy_idx}.")
+          end)
+          Map.update(m, :items, falsy_items, fn e -> e ++ falsy_items end)
+        true ->
+          m
+      end
+      |> then(fn next_monkey -> List.update_at(acc, idx, fn _ -> {next_monkey, idx} end) end)
+    end)
+  end
+
+  def play_round(monkeys) do
+    0..(length(monkeys) - 1)
+    |> Enum.reduce(monkeys, fn idx, acc ->
+      take_turn(acc, idx)
+    end)
+    |> then(fn state ->
+      state
+      |> Enum.map(fn {monkey, idx} -> "Monkey #{idx}: #{Enum.join(monkey.items, ", ")}" end)
+      |> then(fn s -> IO.puts(Enum.join(s, "\n")) end)
+      state
+    end)
+  end
+
+  def play(monkeys, iterations) do
+    1..iterations
+    |> Enum.reduce(monkeys, fn _idx, acc ->
+      play_round(acc)
+    end)
+  end
+
+  def count_item_inspections({monkey, idx}) do
+    {length(monkey.inspected_items), idx}
+  end
+
+  def format_item_inspections({_, _} = monkey_and_index) do
+    monkey_and_index
+    |> count_item_inspections()
+    |> then(fn {num_insections, idx} -> "Monkey #{idx} inspected items #{num_insections} times." end)
+  end
+
+  def sum_most_active(monkeys, top_n) do
+    monkeys
+    |> Enum.map(&AOC.Day11.format_item_inspections/1)
+    |> Enum.join("\n")
+    |> then(fn s -> IO.puts(s) end)
+
+    counts = monkeys
+    |> Enum.map(&AOC.Day11.count_item_inspections/1)
+    |> Enum.sort_by(fn {inspections, _} -> inspections end, :desc)
+    |> Enum.take(top_n)
+
+    {first, _} = List.first(counts)
+    {last, _} = List.last(counts)
+    first * last
   end
 
   def solve(raw_input, 1) do
     parse_input(raw_input)
+    |> play(20)
+    |> sum_most_active(2)
   end
 
   def solve(raw_input, 2) do
