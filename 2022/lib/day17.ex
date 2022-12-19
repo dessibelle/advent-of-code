b1ce5ba2a369b33677e4f1849bbc34700099b180
Simon Fransson <simon.fransson@funnel.io>
2022-12-19 10:02:18 +0100

    add days 14-19

---
+++ 2022/lib/day17.ex
@@ -0,0 +1,16 @@
+defmodule AOC.Day17 do
+
+  def parse_input(raw_input) do
+    raw_input
+    |> String.trim()
+    |> String.split("\n")
+  end
+
+  def solve(raw_input, 1) do
+    parse_input(raw_input)
+  end
+
+  def solve(raw_input, 2) do
+    parse_input(raw_input)
+  end
+end
---
+++ 2022/test/day18_test.exs
@@ -0,0 +1,26 @@
+defmodule AOCTest.Day18Test do
+  use ExUnit.Case
+  alias AOC.Day18, as: Day
+
+  doctest Day
+
+  @test Input.read!(18, "test")
+
+  test "Part 1 (example)" do
+    assert Day.solve(@test, 1) == -1
+  end
+
+  test "Part 2 (example)" do
+    assert Day.solve(@test, 2) == -1
+  end
+
+  # @input Input.read!(18, "input")
+
+  # test "Part 1" do
+  #   assert Day.solve(@input, 1) == -1
+  # end
+
+  # test "Part 2" do
+  #   assert Day.solve(@input, 2) == -1
+  # end
+end
