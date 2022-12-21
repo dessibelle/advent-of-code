f1746a6b47ea5d8fefecdf333c47a26720640df0
Simon Fransson <simon.fransson@funnel.io>
2022-12-21 16:02:24 +0100

    add timing to runner output

--- 2022/lib/runner.ex
+++ 2022/lib/runner.ex
@@ -1,4 +1,16 @@
 defmodule AOC.Runner do
+
+  def format_usecs(u_secs) do
+    cond do
+      u_secs < 1_000 ->
+        "#{u_secs} µs"
+      u_secs < 1_000_000 ->
+        "#{round(u_secs / 1_000)} ms"
+      true ->
+        "#{round(u_secs / 1_000_000)} s"
+    end
+  end
+
   def run!(day) do
     module_path = "Elixir.AOC.Day#{Helpers.pad_leading(day)}"
     IO.puts("== #{module_path} ==")
@@ -6,7 +18,7 @@ defmodule AOC.Runner do
       input = Input.read!(day, mode)
       for part <- 1..2 do
         {u_secs, solution} = :timer.tc(String.to_existing_atom(module_path), :solve, [input, part])
-        IO.puts("#{mode} pt. #{part}: #{solution} (#{u_secs} µs)")
+        IO.puts("#{mode} pt. #{part}: #{solution} (#{format_usecs(u_secs)})")
       end
     end
     IO.puts("")
