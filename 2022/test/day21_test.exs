7504c06013b4d3b5c8c1827d1157e9e779e5c4ac
Simon Fransson <simon.fransson@funnel.io>
2022-12-21 11:20:07 +0100

    add day 21

---
+++ 2022/test/day21_test.exs
@@ -0,0 +1,26 @@
+defmodule AOCTest.Day21Test do
+  use ExUnit.Case
+  alias AOC.Day21, as: Day
+
+  doctest Day
+
+  @test Input.read!(21, "test")
+
+  test "Part 1 (example)" do
+    assert Day.solve(@test, 1) == 152
+  end
+
+  test "Part 2 (example)" do
+    assert Day.solve(@test, 2) == -1
+  end
+
+  # @input Input.read!(21, "input")
+
+  # test "Part 1" do
+  #   assert Day.solve(@input, 1) == -1
+  # end
+
+  # test "Part 2" do
+  #   assert Day.solve(@input, 2) == -1
+  # end
+end
